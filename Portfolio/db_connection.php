699307db255e1b070b2ef8520a88a16c6c4715b9
Dagmm <dagmawihaileraguel@gmail.com>
2025-01-14 06:06:27 +0300

    Update db_connection.php

--- Portfolio/db_connection.php
+++ Portfolio/db_connection.php
@@ -1,1 +1,18 @@
 
+<?php
+class Database {
+    private $dsn = "mysql:host=localhost;dbname=portfolio;charset=utf8";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO($this->dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+}
+?>
